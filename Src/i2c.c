d4a6452d04c57867002203e69057ef4fb8c1dc23
Daniel Trnka <daniel.trnka@gmail.com>
2017-12-12 17:15:04 +0100

    Controlling leds

--- Src/i2c.c
+++ Src/i2c.c
@@ -5,62 +5,59 @@
 #include "messages.h"
 #include "i2c.h"
 
-/*
-osThreadId thread;
+EventGroupHandle_t eventGroup;
 uint8_t data[2];
+uint32_t delay = 500;
+bool running = true;
+int led = 0;
+int res = 0;
 
-void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c) {
-	configASSERT(osThreadResume(thread) == osOK);
-}
-
-void task_i2c(void *i2c) {
-	configASSERT(thread = osThreadGetId());
-
-	for(;;) {
-		HAL_I2C_Slave_Receive_IT(i2c, data, sizeof(data));
-		configASSERT(osThreadSuspend(NULL) == osOK);
-
-		HAL_GPIO_WritePin(GPIOB, data[0], data[1] ? SET : RESET);
-	}
-}
-*/
-
-osThreadId thread;
-uint8_t data[2];
 const int ADDR = 0x20 << 1;
 const int MCP23017_GPIOA = 0x12;
 const int MCP23017_IODIRA = 0x00;
 
-uint32_t delay = 500;
-
 bool led_strip(uint32_t delay_, Direction dir) {
 	delay = delay_;
+
+	xEventGroupSetBits(eventGroup, 0x1);
+	taskYIELD();
 }
 
-void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c) {
-	configASSERT(osThreadResume(thread) == osOK);
+bool led_ctrl(Ctrl ctrl) {
+	if(ctrl == Ctrl_pause) {
+		running = false;
+	} else {
+		running = true;
+
+		if(ctrl == Ctrl_restart) {
+			led = 0;
+		}
+	}
+
+	xEventGroupSetBits(eventGroup, 0x1);
+	taskYIELD();
 }
 
-int res = 0;
 void task_i2c(void *i2c) {
-	configASSERT(thread = osThreadGetId());
+    eventGroup = xEventGroupCreate();
+    configASSERT(eventGroup != NULL);
 
+	// setup all GPIO pins as OUT
 	data[0] =  MCP23017_IODIRA;
 	data[1] = 0x00;
+	while(HAL_I2C_Master_Transmit(i2c, ADDR, data, 2, HAL_MAX_DELAY) != HAL_OK) {
+		osDelay(10);
+	}
 
-	res = HAL_I2C_Master_Transmit(i2c, ADDR, data, 2, HAL_MAX_DELAY);
-	//configASSERT(res == HAL_OK);
-
-	int i = 0;
 	for(;;) {
+		EventBits_t bits = xEventGroupWaitBits(eventGroup, 0x07, pdTRUE, pdFALSE, running ? delay : portMAX_DELAY);
+
 		data[0] =  MCP23017_GPIOA;
-		data[1] = 1 << i;
+		data[1] = running ? 1 << led : 0x00;
 
 		res = HAL_I2C_Master_Transmit(i2c, ADDR, data, 2, HAL_MAX_DELAY);
 
-		i++;
-		i &= 7;
-		osDelay(delay);
-		res++;
+		led++;
+		led &= 7;
 	}
 }
